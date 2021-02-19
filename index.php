474f3f0b61d921bb774b618d5feb65ce4e328f25
nasfernane <nasfernane@gmail.com>
2021-02-19 20:02:38 +0100

    ajout logos

--- index.php
+++ index.php
@@ -8,39 +8,63 @@
     <link rel="stylesheet" href="public/assets/css/style.css">
 </head>
 <body>
-    <header>
-        <h1> Hackaton des légumes oubliés </h1>
-        <h2>Une team, un concept</h2>
-    </header>
+    <div class=mainWrap>
+        <header>
+            <h1>Hackaton des légumes oubliés </h1>
+            <h2>Une team, un concept</h2>
+        </header>
 
-    <section class="binomeSection">
+        <section class="binomeSection">
+        
 
-    <?php
+        <?php
 
-    $binomes = ['Team Panais' => ['Bagrat', 'Ben'], 'Team Cardons' => ['Abdul', 'Yang'], 'Team Rutabagas' => ['Jean-Emmanuel', 'Adam'], 'Team Raiforts' => ['Lima', 'Inès'], 'Team Scorsonères' => ['Mathieu', 'Le'], 'Team Salsifis' => ['Aymen', 'Lazare'], 'Team Capucines' => ['Juliette', 'Amina'], 'Team Pissenlits' => ['Ivan', 'Mathias', 'Gabriel'], 'Team Patissons' => ['Ida', 'Laetitia'], 'Team Ocas' => ['Harris', 'Seif'], 'Team Topinambours' => ['Claude', 'Thierry', 'Nassim']];
+        // définition des binomes
+        $binomes = ['Team Panais' => ['Bagrat', 'Ben'], 'Team Cardons' => ['Abdul', 'Yang'], 'Team Rutabagas' => ['Jean-Emmanuel', 'Adam'], 'Team Raiforts' => ['Lima', 'Inès'], 'Team Scorsonères' => ['Mathieu', 'Le'], 'Team Salsifis' => ['Aymen', 'Lazare'], 'Team Capucines' => ['Juliette', 'Amina'], 'Team Pissenlits' => ['Ivan', 'Mathias', 'Gabriel'], 'Team Patissons' => ['Ida', 'Laetitia'], 'Team Ocas' => ['Harris', 'Seif'], 'Team Topinambours' => ['Claude', 'Thierry', 'Nassim']];
 
-    $brands = ['Coca-Cola', 'Nike', 'Chanel', 'Bentley', 'Justin Bridou', 'Prada', 'Logitech', 'Tesla', 'Guiness', 'Victorinox', 'MSI', 'Razer', 'Etnies', 'Vans', 'Timberland', 'Peugeot', 'Disney', 'Paramount', 'Subway', 'Nvidia', 'Evian', 'Samsung', 'Honor', 'Lu', 'Ankama', 'Hachette', 'Netflix', 'Apple', 'Bose', 'Nicolas', 'Superdry'];
+        // définition des marques
+        $brands = ['Coca-Cola', 'Nike', 'Chanel', 'Bentley', 'Prada', 'Logitech', 'Tesla', 'Guiness', 'Victorinox', 'MSI', 'Razer', 'Etnies', 'Vans', 'Timberland', 'Peugeot', 'Disney', 'Paramount', 'Subway', 'Nvidia', 'Evian', 'Samsung', 'Honor', 'Lu', 'Ankama', 'Hachette', 'Netflix', 'Apple', 'Bose', 'Nicolas', 'Superdry', 'Valrhona', 'Facebook', 'Opquast'];
 
-    
+        // pour chaque binome
+        foreach ($binomes as $binome) {
+            // récupération des variables
+            $randomBrands = array_rand($brands, 2);
+            $firstBrand = $brands[$randomBrands[0]];
+            $secondBrand = $brands[$randomBrands[1]];
+            $team = array_search($binome, $binomes);
 
-    foreach ($binomes as $binome) {
-        $randomBrands = array_rand($brands, 2);
-        $firstBrand = $brands[$randomBrands[0]];
-        $secondBrand = $brands[$randomBrands[1]];
-        $team = array_search($binome, $binomes);
+            // conteneur binome
+            echo "<div class=\"binomeContainer\">";
 
-        echo "<div class=\"binomeContainer\">";
+            if ($binome[2]) {
+                echo "
+                <div class=\"binome\">$team ($binome[0], $binome[1] & $binome[2]) =></div> 
+                <div class=\"brand\">
+                    <div class=\"brand__first\">
+                        <div class=\"brandLogo\" data-logo=\"$firstBrand\"></div>$firstBrand
+                    </div>
+                    <div class=\"brand__second\">
+                        <div class=\"brandLogo\" data-logo=\"$secondBrand\"></div>$secondBrand
+                    </div>
+                </div>";
+            } else {
+                echo "
+                <div class=\"binome\">$team ($binome[0] & $binome[1]) =></div>
+                <div class=\"brand\">
+                    <div class=\"brand__first\">
+                        <div class=\"brandLogo\" data-logo=\"$firstBrand\"></div>$firstBrand
+                    </div>
+                    <div class=\"brand__second\">
+                        <div class=\"brandLogo\" data-logo=\"$secondBrand\"></div>$secondBrand
+                    </div>
+                </div>";
+            } 
 
-        if ($binome[2]) {
-            echo "$team ($binome[0], $binome[1] & $binome[2]) => $firstBrand & $secondBrand";
-        } else {
-            echo "$team ($binome[0] & $binome[1]) => $firstBrand / $secondBrand";
-        } 
-
-        echo "</div>";  
-    }
-    ?>
-    </section>
+            echo "</div>";  
+        }
+        ?>
+        </section>
+    </div>
 
     <script src="public/assets/js/main.js"></script>
 </body>
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,18 +1,11 @@
-const myHeaders = new Headers();
+const logos = document.querySelectorAll('.brandLogo');
 
-myHeaders.append('x-api-key', 'lIdwE5pH3R7iZE5tES4FN1xrppEyM5Sc8wko8UzR');
-myHeaders.append('Content-Type', 'application/json');
+console.log(logos);
 
-let raw = JSON.stringify({ domain: 'https://www.coca-cola-france.fr/nos-marques/coca-cola' });
-
-const requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow',
-};
-
-fetch('https://api.brandfetch.io/v1/logo', requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+window.addEventListener('load', function () {
+    logos.forEach(element => {
+        const path = element.getAttribute('data-logo');
+        console.log(path);
+        element.style.backgroundImage = `url('/public/assets/img/${path}.png')`;
+    });
+});
