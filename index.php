cbb397d2630962de9f6b4dff0e8677b714128a1d
nasfernane <nasfernane@gmail.com>
2021-02-19 17:07:24 +0100

    maj pour hackaton

--- index.php
+++ index.php
@@ -1,5 +1,45 @@
-<?php
-$brands = ['Coca-Cola', 'Nike', 'Chanel', 'Bentley', 'Justin Bridou', 'Prada', 'Logitech', 'Tesla', 'Guiness', 'Victorinox'];
-$randomBrands = array_rand($brands, 3);
-echo $brands[$randomBrands[0]] . "\n" . $brands[$randomBrands[1]];
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simplon Hackaton</title>
+    <link rel="stylesheet" href="public/assets/css/style.css">
+</head>
+<body>
+    <header>
+        <h1> Hackaton des légumes oubliés </h1>
+        <h2>Une team, un concept</h2>
+    </header>
+
+    <section class="binomeSection">
+    <?php
+
+    $binomes = ['Team Panais' => ['Bagrat', 'Ben'], 'Team Cardons' => ['Abdul', 'Yang'], 'Team Rutabagas' => ['Jean-Emmanuel', 'Adam'], 'Team Raiforts' => ['Lima', 'Inès'], 'Team Scorsonères' => ['Mathieu', 'Le'], 'Team Salsifis' => ['Aymen', 'Lazare'], 'Team Capucines' => ['Juliette', 'Amina'], 'Team Pissenlits' => ['Ivan', 'Mathias', 'Gabriel'], 'Team Patissons' => ['Ida', 'Laetitia'], 'Team Ocas' => ['Harris', 'Seif'], 'Team Topinambours' => ['Claude', 'Nassim']];
+
+    $brands = ['Coca-Cola', 'Nike', 'Chanel', 'Bentley', 'Justin Bridou', 'Prada', 'Logitech', 'Tesla', 'Guiness', 'Victorinox', 'MSI', 'Razer', 'Etnies', 'Vans', 'Timberland', 'Peugeot', 'Disney', 'Paramount', 'Subway', 'Nvidia', 'Evian', 'Samsung', 'Honor', 'Lu', 'Ankama', 'Hachette', 'Netflix', 'Apple', 'Bose', 'Nicolas', 'Superdry'];
+
+    foreach ($binomes as $binome) {
+        $randomBrands = array_rand($brands, 2);
+        $firstBrand = $brands[$randomBrands[0]];
+        $secondBrand = $brands[$randomBrands[1]];
+        $team = array_search($binome, $binomes);
+
+        echo "<div class=\"binomeContainer\">";
+
+        if ($binome[2]) {
+            echo "$team ($binome[0], $binome[1] & $binome[1]) => $firstBrand & $secondBrand";
+        } else {
+            echo "$team ($binome[0] et $binome[1]) => $firstBrand & $secondBrand";
+        } 
+
+        echo "</div>";  
+    }
+    ?>
+    </section>
+</body>
+</html>
+
+
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '49b02e4f5f5557e99a5a428f0a0346d3a86c37e4',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '49b02e4f5f5557e99a5a428f0a0346d3a86c37e4',
+    ),
+    'altorouter/altorouter' => 
+    array (
+      'pretty_version' => '2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '127f6e96998708a31ef32252985bea82e3b03888',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5232de97ee3b75b0360528dae24e73db49566ab1',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dc3063ba22c2a1fd2f45ed856374d79114998f91',
+    ),
+    'symfony/var-dumper' => 
+    array (
+      'pretty_version' => 'v5.2.3',
+      'version' => '5.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '72ca213014a92223a5d18651ce79ef441c12b694',
+    ),
+  ),
+);
