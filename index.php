998be7d88ce7c9fd3f94adc4d6ed69fef1b1e37f
nasfernane <nasfernane@gmail.com>
2021-02-20 01:55:11 +0100

    ajout marques

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
         $binomes = ['Team Panais' => ['Bagrat', 'Ben'], 'Team Cardons' => ['Abdul', 'Yang'], 'Team Rutabagas' => ['Jean-Emmanuel', 'Adam'], 'Team Raiforts' => ['Lima', 'Inès'], 'Team Scorsonères' => ['Mathieu', 'Le'], 'Team Salsifis' => ['Aymen', 'Lazare'], 'Team Capucines' => ['Juliette', 'Amina'], 'Team Pissenlits' => ['Ivan', 'Mathias', 'Gabriel'], 'Team Patissons' => ['Ida', 'Laetitia'], 'Team Ocas' => ['Harris', 'Seif'], 'Team Topinambours' => ['Claude', 'Thierry', 'Nassim']];
 
         // définition des marques
-        $brands = ['Coca-Cola', 'Nike', 'Chanel', 'Bentley', 'Prada', 'Logitech', 'Tesla', 'Guiness', 'Victorinox', 'MSI', 'Razer', 'Etnies', 'Vans', 'Timberland', 'Peugeot', 'Disney', 'Paramount', 'Subway', 'Nvidia', 'Evian', 'Samsung', 'Honor', 'Lu', 'Ankama', 'Hachette', 'Netflix', 'Apple', 'Bose', 'Nicolas', 'Superdry', 'Valrhona', 'Facebook', 'Opquast'];
+        $brands = ['Coca-Cola', 'Nike', 'Chanel', 'Bentley', 'Prada', 'Logitech', 'Tesla', 'Guiness', 'Victorinox', 'MSI', 'Etnies', 'Vans', 'Timberland', 'Peugeot', 'Disney', 'Paramount', 'Subway', 'Nvidia', 'Evian', 'Samsung', 'Honor', 'Lu', 'Ankama', 'Hachette', 'Netflix', 'Apple', 'Bose', 'Nicolas', 'Superdry', 'Valrhona', 'Facebook', 'Opquast', 'Mc Donald’s', 'Ikea', 'Zara', 'Kleenex', 'Heinz', 'Saupiquet', 'Bitcoin', 'Tinder', 'Instagram', 'Space X', 'Swatch', 'Fagor', 'Steak ’n Shake', 'Sephora', 'BFM TV', 'LIDL', 'Labeyrie', 'Ebay', 'Michelin', 'Boeing', 'Just Eat', 'Uber', 'Moulinex', 'Cochonou', 'Tefal', 'Porsche', 'Doctissimo', 'Biogaran', 'Oxford', 'Opinel', 'Thermomix', 'Dyson', 'Tamagotchi', 'PlayStation', 'Jack Daniels'];
 
         // pour chaque binome
         foreach ($binomes as $binome) {
